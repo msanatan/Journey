13b2c5ac610cc72f6035e9d539f206354524a550
Marcus Sanatan <msanatan@gmail.com>
2022-08-12 13:26:30 -0400

    Remove narrative text when player no longer collides

--- scripts/StoryTrigger.gd
+++ scripts/StoryTrigger.gd
@@ -8,3 +8,7 @@ export(String, MULTILINE) var story_text = ''
 func _on_StoryTrigger_body_entered(body:Node):
     if body.name == 'Player':
         emit_signal('change_story_text', story_text)
+
+func _on_StoryTrigger_body_exited(body:Node):
+    if body.name == 'Player':
+        emit_signal('change_story_text', '')
