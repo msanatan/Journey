627c2539f8a5b3f8b8e8619181f6f2fa83277430
Marcus Sanatan <msanatan@gmail.com>
2022-08-13 23:16:20 -0400

    Add is on floor check before changing text
    Closer to the original GDevelop version

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -5,5 +5,5 @@ signal game_complete(game_complete_text)
 export(String, MULTILINE) var story_text = 'Finally becomes worth it.\n\nYou made it'
 
 func _on_Area2D_body_entered(body:Node):
-    if body.name == 'Player':
+    if body.name == 'Player' and body.is_on_floor():
         emit_signal('game_complete', story_text)
